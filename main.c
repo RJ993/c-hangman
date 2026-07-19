22ef7e9578c26fc4761f9a4ef641c1a8d43df3ff
RJ993 <196080894+RJ993@users.noreply.github.com>
2026-07-19 15:02:21 -0400

    Create blanks for game

--- main.c
+++ main.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 #include "lib/gen_word.c"
 
+void blanksGen(char *word, char *interface);
+void interfacePrinter(char *interface);
+
 int main(void){
     srand(time(NULL));
+
+    bool gameRunning = false;
     char word[13] = {0};
     int generationReturn = generateWord(word);
     if (generationReturn != 0) return 1;
+    char blanksInterface[13] = {0};
+    blanksGen(word, blanksInterface);
+
     printf("%s\n", word);
+    do {
+        interfacePrinter(blanksInterface);
+    }while(gameRunning);
     return 0;
+}
+
+void blanksGen(char *word, char *interface){
+    for (int i = 0; i < strlen(word); i++){
+        interface[i] = '_';
+    }
+}
+void interfacePrinter(char *interface){
+    for (int i = 0; i < strlen(interface); i++){
+        printf("%c ", interface[i]);
+    }
+    printf("\n");
 }
